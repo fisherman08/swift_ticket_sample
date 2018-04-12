2366deff9b4cde5165727b92764b7d6a415abbcb
ky <orenoikizama@hotmail.com>
2018-04-12 09:30:22 +0900

    test

---
+++ swift_ticket_sample/swift_ticket_sample/views/TicketViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TicketViewController.swift
+//  swift_ticket_sample
+//
+//
+
+import UIKit
+
+class TicketViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // 自動回転させるか否かを返却
+    override var shouldAutorotate: Bool {
+        get {
+            return false
+        }
+    }
+
+    
+    // 表示可能な画面の表示向きを返却
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        get {
+            return .landscapeLeft
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
